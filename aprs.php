30ab4d5cbed04b2075d3f7578017b847693dd6c3
root <root@osm.unizar.es>
2018-02-23 17:20:11 +0100

    Adeed silly tunnel to aprs.fi

---
+++ aprs.php
@@ -0,0 +1,5 @@
+<?php
+   $json = file_get_contents('https://api.aprs.fi/api/get?name=EA2DVJ-11&what=loc&apikey=109984.vUFksEiAq8PAsJQ&format=json');
+echo $json;
+   //$obj = json_decode($json);
+   //var_dump($obj);
