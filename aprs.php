4d42a1e572cf3187691d8ff5ebf436f48551382d
Alejandro Suarez <alejandroscf@gmail.com>
2022-05-13 15:34:24 +0200

    Simplified code and added auto refresh

--- aprs.php
+++ aprs.php
@@ -1,5 +1,6 @@
 <?php
-   $json = file_get_contents('https://api.aprs.fi/api/get?name=EA2DVJ-11&what=loc&apikey=109984.vUFksEiAq8PAsJQ&format=json');
+   $info['indicativo'] = $_GET['indicativo'];
+   $json = file_get_contents('https://api.aprs.fi/api/get?name=' . $info['indicativo'] . '&what=loc&apikey=109984.rYo3Kzj4ISTJzFv&format=json');
 echo $json;
    //$obj = json_decode($json);
    //var_dump($obj);
