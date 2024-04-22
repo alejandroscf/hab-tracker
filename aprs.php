1c83fd8a679b41cc0316796f2eb1bd37c400ab06
Alejandro Suarez <alejandroscf@gmail.com>
2024-04-22 17:17:14 +0200

    Updated predictor API token

--- aprs.php
+++ aprs.php
@@ -1,6 +1,6 @@
 <?php
    $info['indicativo'] = $_GET['indicativo'];
-   $json = file_get_contents('https://api.aprs.fi/api/get?name=' . $info['indicativo'] . '&what=loc&apikey=109984.rYo3Kzj4ISTJzFv&format=json');
+   $json = file_get_contents('https://api.aprs.fi/api/get?name=' . $info['indicativo'] . '&what=loc&apikey=109984.iarHpqxG53wt8&format=json');
 echo $json;
    //$obj = json_decode($json);
    //var_dump($obj);
