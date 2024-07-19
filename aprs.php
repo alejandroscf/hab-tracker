9ace0e8068ffe92aa80e7e54031d9c55a0605abb
Alejandro Suarez <alejandroscf@gmail.com>
2024-07-19 18:50:41 +0200

    Removed APRS.fi key

--- aprs.php
+++ aprs.php
@@ -1,6 +1,7 @@
 <?php
+   include "settings.php";
    $info['indicativo'] = $_GET['indicativo'];
-   $json = file_get_contents('https://api.aprs.fi/api/get?name=' . $info['indicativo'] . '&what=loc&apikey=109984.iarHpqxG53wt8&format=json');
+   $json = file_get_contents('https://api.aprs.fi/api/get?name=' . $info['indicativo'] . '&what=loc&apikey=' . $AprsFiKey . '&format=json');
 echo $json;
    //$obj = json_decode($json);
    //var_dump($obj);
